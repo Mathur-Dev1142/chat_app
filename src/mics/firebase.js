7eb9d2828f7afa42a966374b5d63f6d2ebac01ca
Dev <deepanshumathur456@gmail.com>
2023-07-30 10:42:28 +0530

    initialize firebase into the project and made a misc folder with firebaseJS file

---
+++ src/mics/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCfQGnt6ecE_pNAdW26nsld0VjrsppvT7s',
+
+  authDomain: 'chat--app-e70ad.firebaseapp.com',
+
+  databaseURL: 'https://chat--app-e70ad-default-rtdb.firebaseio.com',
+
+  projectId: 'chat--app-e70ad',
+
+  storageBucket: 'chat--app-e70ad.appspot.com',
+
+  messagingSenderId: '1074460239037',
+
+  appId: '1:1074460239037:web:d8cab18b718602143083d4',
+};
+
+const app = firebase.initializeApp(config);
+
