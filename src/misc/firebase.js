6a8640c8a4958bfa6047dac6de6c9e120db954d9
Dev <deepanshumathur456@gmail.com>
2023-08-08 18:59:51 +0530

    added sigin with google and facebook and interacted with auth and database in firebase

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database'
 
 const config = {
   apiKey: 'AIzaSyCfQGnt6ecE_pNAdW26nsld0VjrsppvT7s',
@@ -17,4 +19,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
 
